baf9ebbd3aa0d5842add5d0d75f5b087e41e3595
Hospice123 <sodehospice@gmail.com>
2024-07-01 04:03:11 +0200

    connect to database

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import SignUp from './pages/SignUp'
+import SignIn from './pages/Signin'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Home from './pages/Home'
+import Header from './components/Header';
+
+
+export default function App() {
+  return (
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/sign-in' element={<SignIn />} />
+  <Route path='/sign-up' element={<SignUp />} />
+  <Route path='/profile' element={<Profile />} />
+  <Route path='/about' element={<About />} />
+   </Routes>
+  </BrowserRouter>
+      
+    
+  )
+}
