b2aa9c73669dbf964110384095e3ebb6c49c8b9e
Hospice123 <sodehospice@gmail.com>
2024-07-24 02:11:26 +0200

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -16,3 +18,5 @@ app.listen(3000, () => {
   console.log(" server is running on port 3000 !!");
 });
 app.use("/api/user", userRouter);
+app.use(express.json());
+app.use("/api/auth", authRouter);
