ebb6d5c90386635d89857cda705d903edfc3d6b5
Hospice123 <sodehospice@gmail.com>
2024-09-20 16:27:47 +0200

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createlisting,
   deleteListing,
-  updateListing
+  updateListing,
+  getListing
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.post("/create", verifyToken, createlisting);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", verifyToken,getListing);
 export default router;
